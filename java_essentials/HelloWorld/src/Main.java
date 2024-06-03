8f3089ec10dab23ad558ba91fea4e5a61665b4b1
vashistpatel-dev <vashist21patel@gmail.com>
2024-06-03 19:16:11 -0400

    Added more print lines.

--- java_essentials/HelloWorld/src/Main.java
+++ java_essentials/HelloWorld/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args)  {
         System.out.println("Hello world! This i Vashist.");
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a dinosaur");
     }
 }
