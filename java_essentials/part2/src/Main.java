39ddf3cbb7f5cc49a806b76f08ba3c3cce75bf2d
vashistpatel-dev <vashist21patel@gmail.com>
2024-06-06 19:35:10 -0400

    Working with integers

--- java_essentials/part2/src/Main.java
+++ java_essentials/part2/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //convert string to int
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+
+        /*String stringNumber = scanner.nextLine();
+        int number = Integer.valueOf(stringNumber);*/
+        //numbers put together
+        int number = Integer.valueOf(scanner.nextInt()); //scanner.nextInt()
+
+        System.out.println("The number you entered as int: " + number);
+
+
+
     }
 }
