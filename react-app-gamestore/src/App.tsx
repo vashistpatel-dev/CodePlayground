b8c882ff1ee151c3b2e5fe7eac713b5e3e1a70ca
vashistpatel-dev <vashist21patel@gmail.com>
2024-06-18 18:31:40 -0400

    Done with listgroup, lets add alert

--- react-app-gamestore/src/App.tsx
+++ react-app-gamestore/src/App.tsx
@@ -1,7 +1,21 @@
 import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><ListGroup /></div>;
+function App() {
+  let items = ["Ney York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
--- react-app-gamestore/src/components/ListGroup.tsx
+++ react-app-gamestore/src/components/ListGroup.tsx
@@ -1,17 +1,42 @@
-function ListGroup() {
-  let items = ["Ney York", "San Francisco", "Tokyo", "London", "Paris"];
-  //   items = [];
+import { MouseEvent, useState } from "react";
 
+// pass {item: [], heading: string}
+interface ListGroupProps {
+  // props should be treated as immutable vs state can be mutable
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: ListGroupProps) {
   const message = items.length === 0 && <p>No item found</p>; //items.length === 0 ? <p>No item found</p> : null;
+  //   const handleClick = (event: MouseEvent) => console.log(event); //type annotation, event handler
+  //   let selectedIndex = 4;
+
+  //Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     /*Fragment from react */
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {message}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item} //can use props.items
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            // onClick={handleClick}
+            onClick={() => {
+              setSelectedIndex(index);
+              //   console.log(selectedIndex);
+              onSelectItem(item);
+            }}
+          >
             {item}
           </li>
         ))}
